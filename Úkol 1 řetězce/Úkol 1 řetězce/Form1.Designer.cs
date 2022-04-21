c17bd08144191160b5673e0a3667af600de21e43
Antoninstolfa <97514759+Antoninstolfa@users.noreply.github.com>
2022-04-21 18:15:18 +0200

    Add files via upload

---
+++ Úkol 1 řetězce/Úkol 1 řetězce/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Úkol_1_řetězce
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.VysledekSouhlasek = new System.Windows.Forms.Label();
+            this.VysledekSamohlasek = new System.Windows.Forms.Label();
+            this.VysledekNepismen = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.Goldenrod;
+            this.button1.Location = new System.Drawing.Point(133, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Spočítat";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(133, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label1.Location = new System.Drawing.Point(12, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Počet souhlásek:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label2.Location = new System.Drawing.Point(12, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Počet samohlásek:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label3.Location = new System.Drawing.Point(11, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(207, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Počet nepísmenných znaků:";
+            // 
+            // VysledekSouhlasek
+            // 
+            this.VysledekSouhlasek.AutoSize = true;
+            this.VysledekSouhlasek.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.VysledekSouhlasek.ForeColor = System.Drawing.Color.LawnGreen;
+            this.VysledekSouhlasek.Location = new System.Drawing.Point(141, 109);
+            this.VysledekSouhlasek.Name = "VysledekSouhlasek";
+            this.VysledekSouhlasek.Size = new System.Drawing.Size(18, 20);
+            this.VysledekSouhlasek.TabIndex = 5;
+            this.VysledekSouhlasek.Text = "0";
+            // 
+            // VysledekSamohlasek
+            // 
+            this.VysledekSamohlasek.AutoSize = true;
+            this.VysledekSamohlasek.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.VysledekSamohlasek.ForeColor = System.Drawing.Color.LawnGreen;
+            this.VysledekSamohlasek.Location = new System.Drawing.Point(161, 131);
+            this.VysledekSamohlasek.Name = "VysledekSamohlasek";
+            this.VysledekSamohlasek.Size = new System.Drawing.Size(18, 20);
+            this.VysledekSamohlasek.TabIndex = 6;
+            this.VysledekSamohlasek.Text = "0";
+            // 
+            // VysledekNepismen
+            // 
+            this.VysledekNepismen.AutoSize = true;
+            this.VysledekNepismen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.VysledekNepismen.ForeColor = System.Drawing.Color.LawnGreen;
+            this.VysledekNepismen.Location = new System.Drawing.Point(224, 151);
+            this.VysledekNepismen.Name = "VysledekNepismen";
+            this.VysledekNepismen.Size = new System.Drawing.Size(18, 20);
+            this.VysledekNepismen.TabIndex = 7;
+            this.VysledekNepismen.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.LawnGreen;
+            this.label4.Location = new System.Drawing.Point(11, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Napiše cokoliv:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(290, 203);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.VysledekNepismen);
+            this.Controls.Add(this.VysledekSamohlasek);
+            this.Controls.Add(this.VysledekSouhlasek);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label VysledekSouhlasek;
+        private System.Windows.Forms.Label VysledekSamohlasek;
+        private System.Windows.Forms.Label VysledekNepismen;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Úkol 1 řetězce/Úkol 1 řetězce/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Úkol_1_řetězce
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        string samohlasky = "aeiouyáéěíóúůý";
+        string souhlasky = "bcčdďfghjklmnpqrřsštťvwxzž";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int pocitadloSamohlasky = 0, pocitadloSouhlasky = 0, pocitadloNepismen = 0;
+                string retezec = textBox1.Text;
+                textBox1.Text.ToLower();
+                foreach (char znak in retezec)
+                {
+                    if (samohlasky.Contains(znak))
+                    {
+                        pocitadloSamohlasky++;
+                    }
+                    if (souhlasky.Contains(znak))
+                    {
+                        pocitadloSouhlasky++;
+                    }
+                    else
+                    {
+                        pocitadloNepismen++;
+                    }
+                }
+                VysledekSamohlasek.Text = Convert.ToString(pocitadloSamohlasky);
+                VysledekSouhlasek.Text = Convert.ToString(pocitadloSouhlasky);
+                VysledekNepismen.Text = Convert.ToString(pocitadloNepismen);
+            }
+            catch
+            {
+                MessageBox.Show("Zadejte nejaký text.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } 
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.LawnGreen;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Black;
+        }
+    }
+}
